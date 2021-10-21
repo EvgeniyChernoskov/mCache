f718370aff0ef07f205a875ed913e20107a0cb19
EvgeniyChernoskov <chernoskov@mail.ru>
2021-10-21 22:03:20 +0300

    hw2

--- cache.go
+++ cache.go
@@ -2,52 +2,64 @@ package mCache
 
 import (
 	"errors"
+	"sync"
 	"time"
 )
 
 type Item struct {
 	Value      interface{}
-	Created    time.Time
 	Expiration int64
 }
 
 type Cache struct {
 	items      map[string]Item
 	expiration time.Duration
+	mutex      *sync.RWMutex
 }
 
 func NewCache(expiration time.Duration) *Cache {
 	cache := Cache{
 		items:      make(map[string]Item),
 		expiration: expiration,
+		mutex:      new(sync.RWMutex),
 	}
 	return &cache
 }
 
-func (c *Cache) Set(key string, value interface{}) {
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
 	var expiration int64
-	expiration = time.Now().Add(c.expiration).UnixNano()
-
+	if ttl == 0 {
+		ttl = c.expiration
+	}
+	expiration = time.Now().Add(ttl).UnixNano()
+	c.mutex.Lock()
 	c.items[key] = Item{
 		Value:      value,
 		Expiration: expiration,
-		Created:    time.Now(),
 	}
+	c.mutex.Unlock()
 }
 
-func (c *Cache) Get(key string) (interface{}, bool) {
+func (c *Cache) Get(key string) (interface{}, error) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+
 	item, exist := c.items[key]
 	if !exist {
-		return nil, false
+		return nil, errors.New("key not exist")
 	}
 	// Если в момент запроса кеш устарел возвращаем nil
 	if time.Now().UnixNano() > item.Expiration {
-		return nil, false
+		return nil, errors.New("expiration key")
 	}
-	return item.Value, true
+
+	return item.Value, nil
 }
 
 func (c *Cache) Delete(key string) error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
 	if _, exist := c.items[key]; !exist {
 		return errors.New("key not found")
 	}
@@ -63,20 +75,24 @@ func (c *Cache) Clean() {
 }
 
 func (c *Cache) clearItems(keys []string) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	for _, k := range keys {
 		delete(c.items, k)
 	}
 }
 
 func (c *Cache) expiredKeys() (keys []string) {
-	for k, i := range c.items {
-		if time.Now().UnixNano() > i.Expiration {
-			keys = append(keys, k)
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	for i, item := range c.items {
+		if time.Now().UnixNano() > item.Expiration {
+			keys = append(keys, i)
 		}
 	}
 	return
 }
 
-func (c Cache) Size() (size int){
+func (c Cache) Size() (size int) {
 	return len(c.items)
 }
