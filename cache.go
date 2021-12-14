d93399467addea287313111365f7a98fd6052c16
chernoskov <jonik@nout.go>
2021-12-14 11:26:05 +0300

    optimization mCache

--- cache.go
+++ cache.go
@@ -2,7 +2,6 @@ package mCache
 
 import (
 	"errors"
-	"fmt"
 	"sync"
 	"time"
 )
@@ -40,75 +39,33 @@ func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
 		Value:      value,
 		Expiration: expiration,
 	}
-
 }
 
 func (c *Cache) Get(key string) (interface{}, error) {
-	c.mutex.RLock()
-	defer c.mutex.RUnlock()
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 
 	item, exist := c.items[key]
 	if !exist {
 		return nil, errors.New("key not exist")
 	}
-	if time.Now().UnixNano() > item.Expiration {
-		// Если в момент запроса кеш устарел возвращаем nil
-		delete(c.items, key) //и сразу удаляем его
-		return nil, errors.New("expiration key")
-	}
 	return item.Value, nil
-}
-
-func (c *Cache) Delete(key string) error {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	if _, exist := c.items[key]; !exist {
-		return errors.New("key not found")
-	}
-	delete(c.items, key)
-	return nil
 }
 
 func (c *Cache) Clean() {
 	//находим элементы с истекшим временем жизни и удаляем из хранилища
-	if keys := c.expiredKeys(); len(keys) != 0 {
-		c.clearItems(keys)
-	}
-}
-
-func (c *Cache) clearItems(keys []string) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	for _, k := range keys {
-		delete(c.items, k)
-	}
-}
-
-func (c *Cache) expiredKeys() (keys []string) {
-	c.mutex.RLock()
-	defer c.mutex.RUnlock()
-	for i, item := range c.items {
+	for key, item := range c.items {
 		if time.Now().UnixNano() > item.Expiration {
-			keys = append(keys, i)
+			delete(c.items, key)
 		}
 	}
-	return
-}
-
-func (c *Cache) Size() (size int) {
-	c.mutex.RLock()
-	defer c.mutex.RUnlock()
-	return len(c.items)
 }
 
 func (c *Cache) runCleanCache(interval time.Duration) {
-		for {
-			select {
-			case <-time.NewTicker(interval).C:
-				c.Clean()
-				fmt.Println("clean, size:", c.Size()) //это только чтобы посмотреть как работает
-			}
+	for {
+		select {
+		case <-time.NewTicker(interval).C:
+			c.Clean()
 		}
+	}
 }
