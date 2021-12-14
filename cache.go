ef7a8ee2186053906dedde51b573837ff8c3b09b
chernoskov <jonik@nout.go>
2021-12-14 12:36:24 +0300

    optimization2

--- cache.go
+++ cache.go
@@ -6,66 +6,62 @@ import (
 	"time"
 )
 
-type Item struct {
-	Value      interface{}
-	Expiration int64
-}
+var ErrItemNotFound = errors.New("cache: item not found")
 
-type Cache struct {
-	items      map[string]Item
-	expiration time.Duration
-	mutex      *sync.RWMutex
-}
-
-func New(expiration time.Duration, cleanInterval time.Duration) *Cache {
-	cache := &Cache{
-		items:      make(map[string]Item),
-		expiration: expiration,
-		mutex:      new(sync.RWMutex),
+type (
+	Cache struct {
+		cache map[interface{}]*item
+		sync.RWMutex
 	}
-	go cache.runCleanCache(cleanInterval)
-	return cache
+
+	item struct {
+		Value      interface{}
+		Expiration int64
+	}
+)
+
+func New(cleanInterval time.Duration) *Cache {
+	с := &Cache{cache: make(map[interface{}]*item)}
+	go с.runCleanCache(cleanInterval)
+	return с
 }
 
-func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	if ttl == 0 {
-		ttl = c.expiration
-	}
-	expiration := time.Now().Add(ttl).UnixNano()
-	c.items[key] = Item{
+func (c *Cache) Set(key interface{}, value interface{}, ttl time.Duration) {
+	c.Lock()
+	c.cache[key] = &item{
 		Value:      value,
-		Expiration: expiration,
+		Expiration: time.Now().Add(ttl).UnixNano(),
 	}
+	c.Unlock()
 }
 
 func (c *Cache) Get(key string) (interface{}, error) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
 
-	item, exist := c.items[key]
+	c.RLock()
+	item, exist := c.cache[key]
+	c.RUnlock()
+
 	if !exist {
-		return nil, errors.New("key not exist")
+		return nil, ErrItemNotFound
 	}
 	return item.Value, nil
-}
-
-func (c *Cache) Clean() {
-	//находим элементы с истекшим временем жизни и удаляем из хранилища
-	for key, item := range c.items {
-		if time.Now().UnixNano() > item.Expiration {
-			delete(c.items, key)
-		}
-	}
 }
 
 func (c *Cache) runCleanCache(interval time.Duration) {
 	for {
 		select {
 		case <-time.NewTicker(interval).C:
-			c.Clean()
+			c.clean()
 		}
 	}
+}
+
+func (c *Cache) clean() {
+	c.Lock()
+	for key, item := range c.cache {
+		if time.Now().UnixNano() > item.Expiration {
+			delete(c.cache, key)
+		}
+	}
+	c.Unlock()
 }
